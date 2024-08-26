bb0ded16b058ee6e3c07748031d455d7c6178625
tiger121005 <tiger121005@github.com\nalkvjn>
2024-08-26 14:22:36 +0900

    correct 5

---
+++ submit/src-5/main/java/app/doggy/checkworkdebug_count/MainActivity.kt
@@ -0,0 +1,29 @@
+package app.doggy.checkworkdebug_count
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.doggy.checkworkdebug_count.databinding.ActivityMainBinding
+
+/**
+ * 問題5
+ * MainActivity.ktのみを修正して、アプリを起動してください。
+ */
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var count: Int = 0
+
+        binding.plusButton.setOnClickListener {
+            count += 1
+            binding.countText.text = count.toString()
+        }
+    }
+}
